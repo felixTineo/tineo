c6e41d3ac963105baafcc5607eeaff726899f2ad
felix <felixtineo05@gmail.com>
2020-04-30 19:57:37 -0400

    Updates

---
+++ precache-manifest.fbcc35d4b612a3412dc8f3d39f0cbc99.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2688ca3816d75fbebf156745f2ae302b",
+    "url": "/tineo/index.html"
+  },
+  {
+    "revision": "8e6077dc19c0df58ee89",
+    "url": "/tineo/static/css/2.53ef695e.chunk.css"
+  },
+  {
+    "revision": "451d5167be8f407069be",
+    "url": "/tineo/static/css/main.dc591d3d.chunk.css"
+  },
+  {
+    "revision": "8e6077dc19c0df58ee89",
+    "url": "/tineo/static/js/2.9534e9d7.chunk.js"
+  },
+  {
+    "revision": "977abce34d75c8f4c04d551445b67e0b",
+    "url": "/tineo/static/js/2.9534e9d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "451d5167be8f407069be",
+    "url": "/tineo/static/js/main.68edeb83.chunk.js"
+  },
+  {
+    "revision": "cf3a17e871ea3bb1a352",
+    "url": "/tineo/static/js/runtime-main.9fa9fdac.js"
+  },
+  {
+    "revision": "29f61e7ee3fbc37d0da8bafdb0a34463",
+    "url": "/tineo/static/media/about.29f61e7e.jpg"
+  },
+  {
+    "revision": "df9d1a62c450015f6f294756d7589778",
+    "url": "/tineo/static/media/amazonaws.df9d1a62.svg"
+  },
+  {
+    "revision": "64c434392a32f31016e88efb01bb68e5",
+    "url": "/tineo/static/media/contact.64c43439.jpg"
+  },
+  {
+    "revision": "cc3ce5d3388869e3036e33f9383b125f",
+    "url": "/tineo/static/media/copyright.cc3ce5d3.svg"
+  },
+  {
+    "revision": "b675af6016eed403de51899be75efe59",
+    "url": "/tineo/static/media/css3.b675af60.svg"
+  },
+  {
+    "revision": "cb32eab3428b24c377cfaf3ec960ba7b",
+    "url": "/tineo/static/media/footer-works.cb32eab3.jpg"
+  },
+  {
+    "revision": "b73214e3ebad909ef6891be949c39947",
+    "url": "/tineo/static/media/git.b73214e3.svg"
+  },
+  {
+    "revision": "d408f1092990152c0af2029b108e0e9e",
+    "url": "/tineo/static/media/graphql.d408f109.svg"
+  },
+  {
+    "revision": "02ac04dc9800d475a4bff8de3af6534a",
+    "url": "/tineo/static/media/greenlink-desk.02ac04dc.jpg"
+  },
+  {
+    "revision": "ebbc5e9d129d8d4a403fe3daba4d3990",
+    "url": "/tineo/static/media/greenlink-movil.ebbc5e9d.jpg"
+  },
+  {
+    "revision": "b735d0648c05b6c76b821949ab4f9abb",
+    "url": "/tineo/static/media/hero.b735d064.jpg"
+  },
+  {
+    "revision": "3056bf2f96c40cd0a814b355959a6983",
+    "url": "/tineo/static/media/html5.3056bf2f.svg"
+  },
+  {
+    "revision": "95c07d5d563027ee67932cfdffc0cbe8",
+    "url": "/tineo/static/media/java.95c07d5d.svg"
+  },
+  {
+    "revision": "1f6069572c9380a733700436d2980f8a",
+    "url": "/tineo/static/media/javascript.1f606957.svg"
+  },
+  {
+    "revision": "f1f3bc7d11ab2d5639e8564c5491cbbc",
+    "url": "/tineo/static/media/laptop-code.f1f3bc7d.svg"
+  },
+  {
+    "revision": "b7d465d80316e8ec4d2c0a6b2243d509",
+    "url": "/tineo/static/media/linux.b7d465d8.svg"
+  },
+  {
+    "revision": "2f0e158681f223be2f12192a4bb1715b",
+    "url": "/tineo/static/media/map-marker-alt.2f0e1586.svg"
+  },
+  {
+    "revision": "14ba79b7cabda9f508492c342c24c057",
+    "url": "/tineo/static/media/mars.14ba79b7.png"
+  },
+  {
+    "revision": "8f2548d5f68478e29f805bff7694d0d9",
+    "url": "/tineo/static/media/mongodb.8f2548d5.svg"
+  },
+  {
+    "revision": "47d2012c828ff8c2c931db66b6cbd81b",
+    "url": "/tineo/static/media/moon.47d2012c.png"
+  },
+  {
+    "revision": "27b0eaaafe77b376d8bf159a87fe36aa",
+    "url": "/tineo/static/media/mysql.27b0eaaa.svg"
+  },
+  {
+    "revision": "a95348e2401d9d509024d93dc30b946c",
+    "url": "/tineo/static/media/next-dot-js.a95348e2.svg"
+  },
+  {
+    "revision": "e1d7fcc979c1d40ac116c43a21cb97df",
+    "url": "/tineo/static/media/node-dot-js.e1d7fcc9.svg"
+  },
+  {
+    "revision": "47be7439720cee23ae38209479f5a33b",
+    "url": "/tineo/static/media/paper-plane.47be7439.svg"
+  },
+  {
+    "revision": "45f646847bc838dbc219b421f4ebf603",
+    "url": "/tineo/static/media/pen-fancy.45f64684.svg"
+  },
+  {
+    "revision": "aa14fd02b5feb8341fc610d663886752",
+    "url": "/tineo/static/media/phone-alt.aa14fd02.svg"
+  },
+  {
+    "revision": "34cbdb156ea57b239962b2909e0a6d89",
+    "url": "/tineo/static/media/postgresql.34cbdb15.svg"
+  },
+  {
+    "revision": "59c0303dc0d73aadd433bd82c724070c",
+    "url": "/tineo/static/media/python.59c0303d.svg"
+  },
+  {
+    "revision": "a2015d7d8325ae4924bdfea582bef687",
+    "url": "/tineo/static/media/quote-left.a2015d7d.svg"
+  },
+  {
+    "revision": "21153c2bc0d28d5bd1cd7b966c78922a",
+    "url": "/tineo/static/media/raos-desk.21153c2b.jpg"
+  },
+  {
+    "revision": "2b10e97799630ab900467202300f1c67",
+    "url": "/tineo/static/media/raos-movil.2b10e977.jpg"
+  },
+  {
+    "revision": "092e0882358cd09dbe9376b64d0f2a0e",
+    "url": "/tineo/static/media/react.092e0882.svg"
+  },
+  {
+    "revision": "280e514ca216947a428b9fbe0a9e183b",
+    "url": "/tineo/static/media/redux.280e514c.svg"
+  },
+  {
+    "revision": "eff325a812f205d9726277c26623050e",
+    "url": "/tineo/static/media/review-1.eff325a8.jpg"
+  },
+  {
+    "revision": "adc3f803d638c202253abc097a165353",
+    "url": "/tineo/static/media/review-2.adc3f803.jpg"
+  },
+  {
+    "revision": "6962f17caafd514bf88b483ff359a054",
+    "url": "/tineo/static/media/reviews.6962f17c.jpg"
+  },
+  {
+    "revision": "643d0db4b0852bdf249ac130d63d1fd6",
+    "url": "/tineo/static/media/sass.643d0db4.svg"
+  },
+  {
+    "revision": "ee2001fb8fa0303b4fe7fc09538bfc88",
+    "url": "/tineo/static/media/server.ee2001fb.svg"
+  },
+  {
+    "revision": "472abd5c1aae31905a026f0fdb30e847",
+    "url": "/tineo/static/media/shipment-desk.472abd5c.jpg"
+  },
+  {
+    "revision": "b121913ac4f9b0469e5d999da16f9b5c",
+    "url": "/tineo/static/media/shipment-movil.b121913a.jpg"
+  },
+  {
+    "revision": "b6825a39834af8b9c21de8bf445b2a81",
+    "url": "/tineo/static/media/spicy-desk.b6825a39.jpg"
+  },
+  {
+    "revision": "7c1cd9279a9e075dcecb9a03de8f00a2",
+    "url": "/tineo/static/media/spicy-movil.7c1cd927.jpg"
+  },
+  {
+    "revision": "80e7a507bc9c18ec3fb891d291e145c0",
+    "url": "/tineo/static/media/todo-desk.80e7a507.jpg"
+  },
+  {
+    "revision": "be6f53a5c09f78ec5e886f2b8b3f995c",
+    "url": "/tineo/static/media/todo-movil.be6f53a5.jpg"
+  },
+  {
+    "revision": "2adeb6bf291d6514289c127c3d5a46ca",
+    "url": "/tineo/static/media/travel-desk.2adeb6bf.jpg"
+  },
+  {
+    "revision": "aa20023244120eb4d502943884c985d4",
+    "url": "/tineo/static/media/travel-movil.aa200232.jpg"
+  },
+  {
+    "revision": "0bde9e50f2d397fb23e8cade865d8614",
+    "url": "/tineo/static/media/vue-dot-js.0bde9e50.svg"
+  },
+  {
+    "revision": "35cc24635dc9f6ef34e012e9157cb752",
+    "url": "/tineo/static/media/winner.35cc2463.svg"
+  },
+  {
+    "revision": "d960ce447fab10a358b4d3ba62daa1a4",
+    "url": "/tineo/static/media/wordpress.d960ce44.svg"
+  }
+]);
